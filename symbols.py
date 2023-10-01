659f3bcdf1d72d4cc959f062998351a40cfe158e
Todd Proebsting <proebsting@gmail.com>
2023-10-01 13:45:15 -0700

    ugh

--- symbols.py
+++ symbols.py
@@ -131,8 +131,11 @@ class Scope:
 
 
 class Phony_Scope(Scope):
-    def __init__(self) -> None:
-        self.parent = None
+    def __init__(self):
+        pass
+
+    def __repr__(self) -> str:
+        return "Phony_Scope()"
 
 
 # holds parameters
